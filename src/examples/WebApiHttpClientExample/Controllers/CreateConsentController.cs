9c91c3bdfd6db3628fad113346c555e29fa84307
George Chakhidze <0xfeeddeadbeef@gmail.com>
2022-11-25 20:15:11 +0400

    Simplify the WebApi example code

--- src/examples/WebApiHttpClientExample/Controllers/CreateConsentController.cs
+++ src/examples/WebApiHttpClientExample/Controllers/CreateConsentController.cs
@@ -1,167 +1,165 @@
 ﻿#nullable enable
 
-namespace WebApiHttpClientExample.Controllers
+namespace WebApiHttpClientExample.Controllers;
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class CreateConsentController : ControllerBase
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Net.Http;
-    using System.Net.Http.Headers;
-    using System.Text.Json;
-    using System.Text.Json.Serialization;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.Extensions.Logging;
+    private readonly IHttpClientFactory _httpClientFactory;
 
-    [ApiController]
-    [Route("[controller]")]
-    public class CreateConsentController : ControllerBase
+    public CreateConsentController(IHttpClientFactory httpClientFactory)
     {
-        private readonly IHttpClientFactory _httpClientFactory;
+        _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+    }
 
-        public CreateConsentController(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
-        }
+    [HttpGet]
+    public async Task<string> Get()
+    {
+        using var client = _httpClientFactory.CreateClient("TBC.OpenBanking.Client");
+        using var request = new HttpRequestMessage(HttpMethod.Post, "v1/consents");
 
-        [HttpGet]
-        public async Task<string> Get()
-        {
-            using var client = _httpClientFactory.CreateClient("TBC.OpenBanking.Client");
-            using var request = new HttpRequestMessage(HttpMethod.Post, "v1/consents");
+        request.Headers.TryAddWithoutValidation("X-Request-ID", Guid.NewGuid().ToString("N"));
+        request.Headers.TryAddWithoutValidation("TPP-Redirect-URI", "https://test-openbanking.tbcbank.ge/callback");
+        request.Headers.TryAddWithoutValidation("TPP-Explicit-Authorisation-Preferred", "false");
+        request.Headers.TryAddWithoutValidation("PSU-IP-Address", "127.0.0.1");
+        request.Headers.TryAddWithoutValidation("PSU-Accept-Language", "en");
+        request.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
 
-            request.Headers.TryAddWithoutValidation("X-Request-ID", Guid.NewGuid().ToString("N"));
-            request.Headers.TryAddWithoutValidation("TPP-Redirect-URI", "https://test-openbanking.tbcbank.ge/callback");
-            request.Headers.TryAddWithoutValidation("TPP-Explicit-Authorisation-Preferred", "false");
-            request.Headers.TryAddWithoutValidation("PSU-IP-Address", "127.0.0.1");
-            request.Headers.TryAddWithoutValidation("PSU-Accept-Language", "en");
-            request.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
-
-            request.Content = new StringContent(
-                JsonSerializer.Serialize(
-                    new Consents
+        request.Content = new StringContent(
+            JsonSerializer.Serialize(
+                new Consents
+                {
+                    CombinedServiceIndicator = false,
+                    FrequencyPerDay = 3,
+                    ValidUntil = DateTimeOffset.Now.AddDays(10),
+                    RecurringIndicator = true,
+                    Access = new AccountAccess
                     {
-                        CombinedServiceIndicator = false,
-                        FrequencyPerDay = 3,
-                        ValidUntil = DateTimeOffset.Now.AddDays(10),
-                        RecurringIndicator = true,
-                        Access = new AccountAccess
-                        {
-                            Accounts = new List<AccountReference>(0),
-                            Balances = new List<AccountReference>(0),
-                            Transactions = new List<AccountReference>(0),
-                        }
-                    }));
+                        Accounts = new List<AccountReference>(0),
+                        Balances = new List<AccountReference>(0),
+                        Transactions = new List<AccountReference>(0),
+                    }
+                }));
 
-            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+        request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
 
-            using var response = await client.SendAsync(request);
+        using var response = await client.SendAsync(request);
 
-            return await response.Content.ReadAsStringAsync();
-        }
+        return await response.Content.ReadAsStringAsync();
     }
+}
 
-    internal sealed class Consents
+internal sealed class Consents
+{
+    [JsonPropertyName("access")]
+    public AccountAccess? Access { get; set; } = new AccountAccess();
+
+    [JsonPropertyName("recurringIndicator")]
+    public bool RecurringIndicator { get; set; }
+
+    [JsonPropertyName("validUntil")]
+    public DateTimeOffset ValidUntil { get; set; }
+
+    [JsonPropertyName("frequencyPerDay")]
+    public int FrequencyPerDay { get; set; }
+
+    [JsonPropertyName("combinedServiceIndicator")]
+    public bool? CombinedServiceIndicator { get; set; }
+
+    private IDictionary<string, object> _additionalProperties = new Dictionary<string, object>();
+
+    [JsonExtensionData]
+    public IDictionary<string, object> AdditionalProperties
     {
-        [JsonPropertyName("access")]
-        public AccountAccess? Access { get; set; } = new AccountAccess();
-
-        [JsonPropertyName("recurringIndicator")]
-        public bool RecurringIndicator { get; set; }
-
-        [JsonPropertyName("validUntil")]
-        public DateTimeOffset ValidUntil { get; set; }
-
-        [JsonPropertyName("frequencyPerDay")]
-        public int FrequencyPerDay { get; set; }
-
-        [JsonPropertyName("combinedServiceIndicator")]
-        public bool? CombinedServiceIndicator { get; set; }
-
-        private IDictionary<string, object> _additionalProperties = new Dictionary<string, object>();
-
-        [JsonExtensionData]
-        public IDictionary<string, object> AdditionalProperties
-        {
-            get { return _additionalProperties; }
-            set { _additionalProperties = value; }
-        }
+        get { return _additionalProperties; }
+        set { _additionalProperties = value; }
     }
+}
 
-    internal sealed class AccountAccess
+internal sealed class AccountAccess
+{
+    [JsonPropertyName("accounts")]
+    public ICollection<AccountReference>? Accounts { get; set; }
+
+    [JsonPropertyName("balances")]
+    public ICollection<AccountReference>? Balances { get; set; }
+
+    [JsonPropertyName("transactions")]
+    public ICollection<AccountReference>? Transactions { get; set; }
+
+    [JsonPropertyName("additionalInformation")]
+    public AdditionalInformationAccess? AdditionalInformation { get; set; }
+
+    [JsonPropertyName("restrictedTo")]
+    public ICollection<string>? RestrictedTo { get; set; }
+
+    private IDictionary<string, object> _additionalProperties = new Dictionary<string, object>();
+
+    [JsonExtensionData]
+    public IDictionary<string, object> AdditionalProperties
     {
-        [JsonPropertyName("accounts")]
-        public ICollection<AccountReference>? Accounts { get; set; }
-
-        [JsonPropertyName("balances")]
-        public ICollection<AccountReference>? Balances { get; set; }
-
-        [JsonPropertyName("transactions")]
-        public ICollection<AccountReference>? Transactions { get; set; }
-
-        [JsonPropertyName("additionalInformation")]
-        public AdditionalInformationAccess? AdditionalInformation { get; set; }
-
-        [JsonPropertyName("restrictedTo")]
-        public ICollection<string>? RestrictedTo { get; set; }
-
-        private IDictionary<string, object> _additionalProperties = new Dictionary<string, object>();
-
-        [JsonExtensionData]
-        public IDictionary<string, object> AdditionalProperties
-        {
-            get { return _additionalProperties; }
-            set { _additionalProperties = value; }
-        }
+        get { return _additionalProperties; }
+        set { _additionalProperties = value; }
     }
+}
 
-    internal sealed class AccountReference
+internal sealed class AccountReference
+{
+    [JsonPropertyName("iban")]
+    public string? Iban { get; set; }
+
+    [JsonPropertyName("bban")]
+    public string? Bban { get; set; }
+
+    [JsonPropertyName("pan")]
+    public string? Pan { get; set; }
+
+    [JsonPropertyName("maskedPan")]
+    public string? MaskedPan { get; set; }
+
+    [JsonPropertyName("msisdn")]
+    public string? Msisdn { get; set; }
+
+    [JsonPropertyName("currency")]
+    public string? Currency { get; set; }
+
+    [JsonPropertyName("cashAccountType")]
+    public string? CashAccountType { get; set; }
+
+    private IDictionary<string, object> _additionalProperties = new Dictionary<string, object>();
+
+    [JsonExtensionData]
+    public IDictionary<string, object> AdditionalProperties
     {
-        [JsonPropertyName("iban")]
-        public string? Iban { get; set; }
-
-        [JsonPropertyName("bban")]
-        public string? Bban { get; set; }
-
-        [JsonPropertyName("pan")]
-        public string? Pan { get; set; }
-
-        [JsonPropertyName("maskedPan")]
-        public string? MaskedPan { get; set; }
-
-        [JsonPropertyName("msisdn")]
-        public string? Msisdn { get; set; }
-
-        [JsonPropertyName("currency")]
-        public string? Currency { get; set; }
-
-        [JsonPropertyName("cashAccountType")]
-        public string? CashAccountType { get; set; }
-
-        private IDictionary<string, object> _additionalProperties = new Dictionary<string, object>();
-
-        [JsonExtensionData]
-        public IDictionary<string, object> AdditionalProperties
-        {
-            get { return _additionalProperties; }
-            set { _additionalProperties = value; }
-        }
+        get { return _additionalProperties; }
+        set { _additionalProperties = value; }
     }
+}
 
-    internal sealed class AdditionalInformationAccess
+internal sealed class AdditionalInformationAccess
+{
+    [JsonPropertyName("ownerName")]
+    public ICollection<AccountReference>? OwnerName { get; set; }
+
+    [JsonPropertyName("trustedBeneficiaries")]
+    public ICollection<AccountReference>? TrustedBeneficiaries { get; set; }
+
+    private IDictionary<string, object> _additionalProperties = new Dictionary<string, object>();
+
+    [JsonExtensionData]
+    public IDictionary<string, object> AdditionalProperties
     {
-        [JsonPropertyName("ownerName")]
-        public ICollection<AccountReference>? OwnerName { get; set; }
-
-        [JsonPropertyName("trustedBeneficiaries")]
-        public ICollection<AccountReference>? TrustedBeneficiaries { get; set; }
-
-        private IDictionary<string, object> _additionalProperties = new Dictionary<string, object>();
-
-        [JsonExtensionData]
-        public IDictionary<string, object> AdditionalProperties
-        {
-            get { return _additionalProperties; }
-            set { _additionalProperties = value; }
-        }
+        get { return _additionalProperties; }
+        set { _additionalProperties = value; }
     }
 }
--- src/examples/WebApiHttpClientExample/Startup.cs
+++ src/examples/WebApiHttpClientExample/Startup.cs
@@ -1,112 +1,92 @@
-﻿namespace WebApiHttpClientExample
+﻿namespace WebApiHttpClientExample;
+
+using System;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using TBC.OpenBanking.Jws;
+using TBC.OpenBanking.Jws.Http;
+
+public class Startup
 {
-    using System;
-    using System.Net;
-    using System.Net.Http;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Caching.Memory;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
-    using Microsoft.Extensions.Logging;
-    using Microsoft.Extensions.Options;
-    using Microsoft.OpenApi.Models;
-    using TBC.OpenBanking.Jws;
-    using TBC.OpenBanking.Jws.Http;
-
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        //
+        // Read JWS options from IConfiguration
+        //
+        services.AddOptions<JwsClientOptions>().BindConfiguration("Jws:Options");
+
+        services.AddControllers();
+
+        services.AddSwaggerGen(c =>
         {
-            Configuration = configuration;
-        }
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiHttpClientExample", Version = "v1" });
+        });
 
-        public IConfiguration Configuration { get; }
+        //
+        // Memory cache is required by JwsMessageHandler
+        //
+        services.AddMemoryCache();
 
-        public void ConfigureServices(IServiceCollection services)
+        //
+        // JwsMessageHandler is used to sign requests and validate responses
+        //
+        services.AddTransient<JwsMessageHandler>();
+
+        //
+        // HttpClient for TBC Bank's Open Banking API
+        //
+        services.AddHttpClient(name: "TBC.OpenBanking.Client", configureClient: (client) =>
         {
-            //
-            // Add ability to parse and convert cert:// and pfx:// URLs
-            // (Required for JwsClientOptions deserialization from appsettings to work)
-            //
-            X509CertificateLocator.RegisterUriParsers();
-
-            //
-            // Read JWS options from appsettings.json
-            //
-            services.AddOptions<JwsClientOptions>()
-                .BindConfiguration("Jws:Options");
-
-            services.AddControllers();
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiHttpClientExample", Version = "v1" });
-            });
-
-            //
-            // Memory cache is required by JwsMessageHandler
-            //
-            services.AddMemoryCache();
-
-            //
-            // HttpClient for TBC Bank's Open Banking API
-            //
-            services.AddHttpClient(name: "TBC.OpenBanking.Client", configureClient: (client) =>
-            {
-                client.BaseAddress = new Uri(this.Configuration["OpenBankingConnection:BaseUrl"]);
-            }).ConfigurePrimaryHttpMessageHandler((svcs) =>
+            client.BaseAddress = new Uri(this.Configuration["OpenBankingConnection:BaseUrl"]);
+        })
+            .ConfigurePrimaryHttpMessageHandler((svcs) =>
             {
                 var handler = new HttpClientHandler
                 {
                     AutomaticDecompression = DecompressionMethods.All,
-                    MaxConnectionsPerServer = 4,
                     UseCookies = false,
-                    UseProxy = false,
                 };
 
                 handler.ClientCertificateOptions = ClientCertificateOption.Manual;
-                handler.ClientCertificates.Add(
-                    new X509CertificateLocator(
-                        new Uri(this.Configuration["OpenBankingConnection:ClientCertificate"])).GetCertificate());
+                handler.ClientCertificates.Add(new X509CertificateLocator(
+                    new Uri(this.Configuration["OpenBankingConnection:ClientCertificate"])).GetCertificate());
 
+                // DONT use this in production code:
                 handler.ServerCertificateCustomValidationCallback =
                     HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
 
                 return handler;
             })
-            .AddHttpMessageHandler((svcs) =>
-            {
-                //
-                // Insert JWS message handler into the handler pipeline
-                // JwsMessageHandler will sign requests and validate responses
-                //
+            .AddHttpMessageHandler<JwsMessageHandler>();  // Handler to sign requests and validate responses
+    }
 
-                var options = svcs.GetRequiredService<IOptions<JwsClientOptions>>();
-
-                var loggerFactory = svcs.GetRequiredService<ILoggerFactory>();
-                var memoryCache = svcs.GetRequiredService<IMemoryCache>();
-
-                return new JwsMessageHandler(options, loggerFactory, memoryCache);
-            });
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiHttpClientExample v1"));
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiHttpClientExample v1"));
         }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
     }
 }
